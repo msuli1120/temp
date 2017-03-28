bce156758f17bcbd087d0519ed50ec6396cc7836
xl <msuli_1120@ymail.com>
2017-03-28 06:25:23 -0700

    temp

---
+++ app/app.php
@@ -0,0 +1,18 @@
+<?php
+  date_default_timezone_set('America/Los_Angeles');
+  require_once __DIR__."/../vendor/autoload.php";
+  require_once __DIR__."/../src/Class.php";
+
+  $app = new Silex\Application();
+
+  $app->register(new Silex\Provider\TwigServiceProvider(), array(
+    'twig.path' => __DIR__.'/../views'
+  ));
+
+  $app -> get("/", function () use ($app) {
+
+    return $app['twig']->render('index.html.twig');
+  });
+
+  return $app;
+?>
