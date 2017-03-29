1a517d4eec8b1ab56d21f03a86206784b8d55c78
xl <msuli_1120@ymail.com>
2017-03-29 06:22:47 -0700

    add debug

--- app/app.php
+++ app/app.php
@@ -3,13 +3,21 @@
   require_once __DIR__."/../vendor/autoload.php";
   require_once __DIR__."/../src/Class.php";
 
+  use Symfony\Component\Debug\Debug;
+  Debug::enable();
+
   $app = new Silex\Application();
+
+  $app['debug'] = true;
 
   $app->register(new Silex\Provider\TwigServiceProvider(), array(
     'twig.path' => __DIR__.'/../views'
   ));
 
-  $app -> get("/", function () use ($app) {
+  use Symfony\Component\HttpFoundation\Request;
+  Request::enableHttpMethodParameterOverride();
+
+  $app->get("/", function () use ($app) {
 
     return $app['twig']->render('index.html.twig');
   });
